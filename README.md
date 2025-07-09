ce61b5e8db2f6ec5b9732c7c4ab052d6c8af896a
Leonardo D'Agostino <ldaguichi@gmail.com>
2025-07-09 13:27:48 -0300

    feat: run-dbt script (debug working)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a demo of dbt (data build tool) for Claro
 
 This uses uv
+
+export DBT_PROFILES_DIR=$(pwd)/dbt
--- run-dbt.sh
+++ run-dbt.sh
@@ -3,4 +3,7 @@ set -o allexport
 source "$(dirname "$0")/.env"
 set +o allexport
 
+# como el directorio del proyecto está en dbt/demo
+cd "$(dirname "$0")/demo"
+
 uv run dbt "$@"
